55056d2af9670a1e97865cc72ee4f5d0fd8ec130
Dario <salina.dario@gmail.com>
2019-02-13 12:20:37 +0100

    react app created, cors added

---
+++ client/ebay-clone/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import store from './store'
+import {Provider} from 'react-redux'
+import { Route } from 'react-router-dom'
+import Home from './components/Home'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Route path="/" exact component={Home} />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import {createKoaServer} from "routing-controllers"
 import AdvertsController from './adverts/controller'
 import setupDb from './db'
 // import {Action} from 'routing-controllers'
+// import Koa from 'koa';
+// import cors from '@koa/cors'
 
 const port = process.env.PORT || 4000
 
