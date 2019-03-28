d1c622e4fb9aae5529f2a543d884ee8772ab09d0
Dario <salina.dario@gmail.com>
2019-03-28 12:37:01 +0100

    add components and basic styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import store from './store'
-import {Provider} from 'react-redux'
-import { Route } from 'react-router-dom'
-import AdvertsListContainer from './components/AdvertsListContainer'
-import AdvertDetailsContainer from './components/AdvertDetailsContainer'
-import CreateAdvertContainer from './components/CreateAdvertContainer'
+import React, { Component } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import AdvertsListContainer from "./components/AdvertsListContainer";
+import AdvertDetailsContainer from "./components/AdvertDetailsContainer";
+import CreateAdvertContainer from "./components/CreateAdvertContainer";
+import LoginPage from "./components/login/LoginPage";
+import LogOut from "./components/logout/LogOut";
+import SignUpPage from "./components/signup/SignUpPage";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Route path="/" exact component={AdvertsListContainer} />
-          <Route path="/adverts/:id" component={AdvertDetailsContainer} />
-          <Route path="/advert" component={CreateAdvertContainer} />
+        <div class="pa3 pa4-ns">
+          <h1 class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns">
+            Bakeka
+          </h1>
+          <Route path="/" component={Home} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route exact path="/logout" component={LogOut} />
+          <Route exact path="/adverts" exact component={AdvertsListContainer} />
+          <Route exact path="/adverts/:id" component={AdvertDetailsContainer} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/advert" component={CreateAdvertContainer} />
         </div>
       </Provider>
     );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
-  return (<h1>Welcome to ebayClone!</h1>)
+  return (
+    <nav class="tc pb3 center">
+      <div class="link dim gray b f6 f5-ns dib mr3" href="#" title="Home">
+        <Link to={`/signup`}>SignUp</Link>
+      </div>
+      <div class="link dim gray    f6 f5-ns dib mr3" href="#" title="Home">
+        {" "}
+        <Link to={`/login`}>Login</Link>
+      </div>
+      <div class="link dim gray    f6 f5-ns dib mr3" href="#" title="About">
+        <Link to={`/adverts`}>Adverts</Link>
+      </div>
+      <div class="link dim gray    f6 f5-ns dib mr3" href="#" title="Store">
+        <Link to={`/logout`}>LogOut</Link>
+      </div>
+    </nav>
+  );
 }
